2426eacf3d63f4f3e28e97592efd6d45dccdc2d4
L-47 <thelordofblackops2@gmail.com>
2020-07-21 12:05:24 +0200

    removed print

--- glassnode.py
+++ glassnode.py
@@ -17,11 +17,10 @@ api_items = [
 # use index of variables in get requests for ease of use
 assets = api_items[0].values()
 futures = api_items[1].values()
-print(str(futures))
+
 
 class GlassnodeClient:
 
-<<<<<<< HEAD
     def __init__(self):
         self._api_key = os.environ.get("GLASSNODE_API_KEY")
         # check if api key is in env
@@ -74,55 +73,3 @@ class GlassnodeClient:
             return s
         except Exception as e:
             print(e)
-=======
-  def __init__(self):
-    self._api_key = os.environ.get("GLASSNODE_API_KEY")
-    if self._api_key == None | self._api_key == "":
-        os.environ["GLASSNODE_API_KEY"] = input("api_key not found, please enter api_key: ")
-
-  @property
-  def api_key(self):
-    return self._api_key
-
-  def set_api_key(self, value):
-    self._api_key = value
-
-  def get(self, url, a='BTC', i='24h', c='native', s=None, u=None):
-    p = dict()
-    p['a'] = a
-    p['i'] = i
-    p['c'] = c
-
-    if s is not None:
-      try:
-        p['s'] = iso8601.parse_date(s).strftime('%s')
-      except ParseError:
-        p['s'] = s
-
-    if u is not None:
-      try:
-        p['u'] = iso8601.parse_date(u).strftime('%s')
-      except ParseError:
-        p['u'] = s
-
-    p['api_key'] = self.api_key
-
-    r = requests.get(url, params=p)
-
-    try:
-       r.raise_for_status()
-    except Exception as e:
-        print(e)
-        print(r.text)
-
-    try:
-        df = pd.DataFrame(json.loads(r.text))
-        df = df.set_index('t')
-        df.index = pd.to_datetime(df.index, unit='s')
-        df = df.sort_index()
-        s = df.v
-        s.name = '_'.join(url.split('/')[-2:])
-        return s
-    except Exception as e:
-        print(e)
->>>>>>> bf158577901e37fe5f8d8de79f57a3b67cc43e2a
